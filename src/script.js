b5300b4bc7c087b40153558cc20f581f90052706
Amanda <amandaoliveirapolari@gmail.com>
2023-05-04 22:28:40 -0300

    Lógica de colocar o menu mobile dentro de um novo elemento implementada

--- src/script.js
+++ src/script.js
@@ -4,6 +4,27 @@ function menuShow() {
     if (menuMobile.classList.contains('open')) {
         menuMobile.classList.remove('open');
     } else {
+        // Lógica para colocar o meu conteúdo logo em seguida do header:
+        const novoElemento = document.createElement('div');
+        const elementoExistente = document.getElementById('header');
+        elementoExistente.insertAdjacentElement('afterend', novoElemento);
+        const meuConteudo = document.getElementById('menu-para-mobile');
+        novoElemento.appendChild(meuConteudo);
+        // ----
         menuMobile.classList.add('open');
     }
 }
+
+// Lógica do formulário:
+// Limpar formulário:
+const btn = document.getElementById('botao-enviar');
+btn.addEventListener('click', (event) => {
+    event.preventDefault();
+    const inputs = document.querySelectorAll('.inputs-contato');
+    inputs.forEach((input) => {
+        input.value = '';
+    });
+});
+// ------------------
+
+//
